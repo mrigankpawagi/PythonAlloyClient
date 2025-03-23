239b3f16e508f9ba61120bff03fdcc7b8716a3bf
Mrigank Pawagi <mrigankpawagi@gmail.com>
2025-03-23 15:01:08 +0530

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PythonAlloyClient',
-    version='0.1.0',
+    version='0.1.1',
     author='Mrigank Pawagi',
     author_email='mrigankpawagi@gmail.com',
     description='Python Client for the Alloy Language Server',
